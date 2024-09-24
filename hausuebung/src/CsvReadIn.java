9e0ec2cc88bb15a6e205e8144c96a8e16f0b1b99
unknown <allerstorferJ22037@sus.htl-grieskirchen.at>
2024-09-24 15:55:15 +0200

    CsvReadIn Class finished

--- hausuebung/src/CsvReadIn.java
+++ hausuebung/src/CsvReadIn.java
@@ -1,2 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CsvReadIn {
+    String fileName = "Tester.csv";
+    List<int[]> data = new ArrayList<>();
+
+    public void readIn(){
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] werte = line.split("\\s+");
+                int[] lineData = new int[2];
+                lineData[0] = Integer.parseInt(werte[0]);
+                lineData[1] = Integer.parseInt(werte[1]);
+
+                data.add(lineData);
+            }
+        } catch (IOException e) {
+            System.out.println("Fehler beim Lesen der Datei!");
+        }
+    }
+
+
 }
--- hausuebung/src/NumberTester.java
+++ hausuebung/src/NumberTester.java
@@ -1,21 +1,28 @@
 public class NumberTester {
-    NumberTester oddTester;
-    NumberTester primeTester;
-    NumberTester palindromeTester;
+    NumberTest oddTester = (number) -> (number % 2) != 0;
+    NumberTest primeTester = (number) -> {
+        if(number <= 1) return false;
+        for(int i = 0;i <= Math.sqrt(number);i++){
+            if(number % i == 0) return false;
+        }
+        return true;
+    };
+
+    NumberTest palindromeTester;
 
     public NumberTester(String fileName){
 
     }
 
-    public void setOddEvenTester(NumberTester oddTester){
+    public void setOddEvenTester(NumberTest oddTester){
         this.oddTester = oddTester;
     }
 
-    public void setPrimeTester(NumberTester primeTester){
+    public void setPrimeTester(NumberTest primeTester){
         this.primeTester = primeTester;
     }
 
-    public NumberTester(NumberTester palindromeTester) {
+    public NumberTester(NumberTest palindromeTester) {
         this.palindromeTester = palindromeTester;
     }
 }
