df287e6e2b05d8e8ca83a535b5915e3df81dd345
unknown <allerstorferJ22037@sus.htl-grieskirchen.at>
2024-09-24 15:04:06 +0200

    NumberTest Interface created

--- hausuebung/src/NumberTester.java
+++ hausuebung/src/NumberTester.java
@@ -1,2 +1,21 @@
 public class NumberTester {
+    NumberTester oddTester;
+    NumberTester primeTester;
+    NumberTester palindromeTester;
+
+    public NumberTester(String fileName){
+
+    }
+
+    public void setOddEvenTester(NumberTester oddTester){
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTester primeTester){
+        this.primeTester = primeTester;
+    }
+
+    public NumberTester(NumberTester palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
 }
